239128d176dabf5f32a3fd8c24960407e87be957
rydelr <111964945+rydelr@users.noreply.github.com>
2022-08-25 09:17:59 +0200

    license nad readme added

--- private/api_keys.py
+++
@@ -1,4 +0,0 @@
-def keyapi():
-    api_key = 'BmMacXioMr1LGkBgmsplqYEXmzWrdivF6HF9bl7W8wXpeaPQAItgLIVapyyu7BpI'
-    secret_key = 'wp4DujBzoMueuftKEPiuPxPyRDv4DEzyQExMwWX9PUjcHOk2ke3mz6pcS9VsKM6Z'
-    return api_key, secret_key
